7263dd6825ff4c6275210084f7341b7e63f84544
Frank Wazeter <38684085+fwazeter@users.noreply.github.com>
2024-01-21 17:15:11 -0700

    Delete dist directory

--- dist/plugins/route-factory/index.js
+++
@@ -1,8 +0,0 @@
-import { RouteFactory } from './route-factory.js';
-const routeFactory = (fastify, options, done) => {
-    const routeFactory = new RouteFactory(fastify, options);
-    routeFactory.registerRoutes();
-    done();
-};
-export default routeFactory;
-//# sourceMappingURL=index.js.map
--- dist/types/IDatabaseConfig.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=IDatabaseConfig.js.map
